67de77b88c0b3e19ed837289488b034b320de31d
Антон Кондратов <contact@site-home.ru>
2017-11-02 01:21:13 +0300

    Строитель

---
+++ src/Hobocta/Patterns/Builder/BurgerBuilder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Hobocta\Patterns\Builder;
+
+class BurgerBuilder
+{
+    public $size;
+    public $cheese = false;
+    public $pepperoni = false;
+    public $lettuce = false;
+    public $tomato = false;
+
+    public function __construct($size)
+    {
+        $this->size = $size;
+    }
+
+    public function addCheese()
+    {
+        $this->cheese = true;
+
+        return $this;
+    }
+
+    public function addPepperoni()
+    {
+        $this->pepperoni = true;
+
+        return $this;
+    }
+
+    public function addLettuce()
+    {
+        $this->lettuce = true;
+
+        return $this;
+    }
+
+    public function addTomato()
+    {
+        $this->tomato = true;
+
+        return $this;
+    }
+
+    public function build()
+    {
+        return new Burger($this);
+    }
+}
