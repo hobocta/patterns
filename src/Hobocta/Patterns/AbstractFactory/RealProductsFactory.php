8d7b37f4efd9e46937e52694bf07d8aecaee0e80
Антон Кондратов <contact@site-home.ru>
2017-11-02 01:24:08 +0300

    Тест

--- src/Hobocta/Patterns/AbstractFactory/RealProductsFactory.php
+++ src/Hobocta/Patterns/AbstractFactory/RealProductsFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Hobocta\Patterns\AbstractFactory;
 
 class RealProductsFactory extends AbstractFactory
--- test.php
+++ test.php
@@ -1,34 +1,35 @@
 <?php
 require_once 'src/autoload.php';
 
-$singleton = \Hobocta\Patterns\Singleton\Singleton::getInstance();
-
+// Синглтон
+/*$singleton = \Hobocta\Patterns\Singleton\Singleton::getInstance();
 echo '<pre>';
 var_export($singleton);
-echo '</pre>';
+echo '</pre>';*/
 
-
-$realProductsFactory = new \Hobocta\Patterns\AbstractFactory\RealProductsFactory();
-
+// Абстрактная фабрика
+/*$realProductsFactory = new \Hobocta\Patterns\AbstractFactory\RealProductsFactory();
 echo '<pre>';
 var_export($realProductsFactory->createBook()->getName());
-echo '</pre>';
+echo '</pre>';*/
 
-
-$coffee = new \Hobocta\Patterns\Decorator\Coffee();
-
+// Декоратор
+/*$coffee = new \Hobocta\Patterns\Decorator\Coffee();
 echo '<pre>';
 var_export($coffee->getCost());
 echo '</pre>';
-
 $milkCoffee = new \Hobocta\Patterns\Decorator\MilkCoffee($coffee);
-
 echo '<pre>';
 var_export($milkCoffee->getCost());
 echo '</pre>';
-
 $chocolateCoffee = new \Hobocta\Patterns\Decorator\ChocolateCoffee($milkCoffee);
-
 echo '<pre>';
 var_export($chocolateCoffee->getCost());
-echo '</pre>';
+echo '</pre>';*/
+
+// Строитель
+/*$burgerBuilder = new \Hobocta\Patterns\Builder\BurgerBuilder(12);
+$burger = $burgerBuilder->addCheese()->addLettuce()->build();
+echo '<pre>';
+var_export($burger);
+echo '</pre>';*/
